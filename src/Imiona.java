1a48d892691244d559253c527c3851f3a3457c1d
agatito <agatito@yopmail.com>
2018-01-23 16:56:29 +0100

    wyświetlenie imion z tablicy

---
+++ src/Imiona.java
@@ -0,0 +1,10 @@
+public class Imiona {
+    public static void main (String[] args) {
+
+        String[]imiona = {"Franek", "Janek", "Paweł"};
+
+        for (String imie:imiona)
+            System.out.println(imie);
+
+    }
+}
